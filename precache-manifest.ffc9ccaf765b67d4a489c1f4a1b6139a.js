f9af363acdf0d0ceb51dd4c7450e42d33f14f4e0
daryoush12 <daryoush_farsimadan@hotmail.com>
2019-09-23 19:33:40 +0300

    Updates

---
+++ precache-manifest.ffc9ccaf765b67d4a489c1f4a1b6139a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5288ffe456d587b01aff0c103bf87c1",
+    "url": "/git-react-page/index.html"
+  },
+  {
+    "revision": "599495a896dd1f524406",
+    "url": "/git-react-page/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "73fd5964e88c3d4542c6",
+    "url": "/git-react-page/static/js/2.e9186058.chunk.js"
+  },
+  {
+    "revision": "599495a896dd1f524406",
+    "url": "/git-react-page/static/js/main.ad62be8a.chunk.js"
+  },
+  {
+    "revision": "d08b3bb74915ff962a18",
+    "url": "/git-react-page/static/js/runtime-main.0b9f2af0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/git-react-page/static/media/logo.25bf045c.svg"
+  }
+]);
